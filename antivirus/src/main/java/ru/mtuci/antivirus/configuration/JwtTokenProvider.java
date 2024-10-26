1ebf59a8ddbea61b7098db9f9a98a9a2eb53d68e
Maxim <130181963+wumpochuck@users.noreply.github.com>
2024-10-26 17:41:23 +0300

    added some task2

--- antivirus/src/main/java/ru/mtuci/antivirus/configuration/JwtTokenProvider.java
+++ antivirus/src/main/java/ru/mtuci/antivirus/configuration/JwtTokenProvider.java
@@ -1,15 +1,21 @@
 package ru.mtuci.antivirus.configuration;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -26,8 +32,59 @@ public class JwtTokenProvider {
     }
 
     //TODO: реализовать методы создания, валидации и получения информации из JWT токена
-    public String createToken(String username, Set<GrantedAuthority> authorities) {}
-    public boolean validateToken(String token) {}
-    public String getUsername(String token) {}
-    public Set<GrantedAuthority> getAuthorities(String token) {}
+    public String createToken(String username, Set<GrantedAuthority> authorities) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", authorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList())) // Добавляем роли
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    
+    public boolean validateToken(String token, String username) {
+        final String tokenUsername = getUsername(token);
+        return (tokenUsername.equals(username) && !isTokenExpired(token));
+    }
+
+
+    public String getUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+
+    public Set<GrantedAuthority> getAuthorities(String token) {
+        Claims claims = extractAllClaims(token);
+        Collection<String> roles = claims.get("roles", Collection.class);
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
